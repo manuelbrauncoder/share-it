3187daf1b840dc88bb954057369c3a6a4100020f
Manuel Braun <manuel.braun.coder@gmail.com>
2025-04-12 20:43:48 +0200

    fetch the current user from firestore on start-screen

--- src/app/pages/start-screen/start-screen.component.ts
+++ src/app/pages/start-screen/start-screen.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { FirestoreService } from '../../services/firestore.service';
 import { UiService } from '../../services/ui.service';
+import { User } from '../../interfaces/User';
+import { UserHelper } from "../../helpers/UserHelper";
 
 @Component({
   selector: 'app-start-screen',
@@ -13,8 +15,31 @@ export class StartScreenComponent {
   authService = inject(AuthenticationService);
   firestoreService = inject(FirestoreService);
   uiService = inject(UiService);
+  currentUser?: User
 
-  testToast() {
-    this.uiService.setToastMessage(false, 'Test Toast Message');
+  constructor() {
+    this.getCurrentUser();
+  }
+
+  getCurrentUser() {
+    const id = this.authService.auth.currentUser?.uid;
+    if (id) {
+      this.firestoreService.getUserByID(id)
+      .subscribe({
+        next: (data: User | undefined) => {
+          if (data) {
+            this.currentUser = data;
+            console.log(this.currentUser);
+          } else {
+            console.log('user not found');
+          }
+        },
+        error: (err) => {
+          console.log('Error fetching User', err);
+        }
+      });
+    } else {
+      console.log('No User logged in');
+    }
   }
 }
--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,11 +1,19 @@
 import { inject, Injectable } from '@angular/core';
-import { doc, Firestore, setDoc } from '@angular/fire/firestore';
+import {
+  doc,
+  DocumentData,
+  DocumentSnapshot,
+  Firestore,
+  getDoc,
+  setDoc,
+} from '@angular/fire/firestore';
 import { AuthUser } from '../interfaces/AuthUser';
-import { from, Observable } from 'rxjs';
+import { from, map, Observable } from 'rxjs';
 import { UserHelper } from '../helpers/UserHelper';
-import { FirestorePath } from "../enums/FirestorePath";
+import { FirestorePath } from '../enums/FirestorePath';
 import { v4 as uuidv4 } from 'uuid';
 import { Group } from '../interfaces/Group';
+import { User } from '../interfaces/User';
 
 @Injectable({
   providedIn: 'root',
@@ -31,12 +39,33 @@ export class FirestoreService {
   saveGroup(group: Group): Observable<void> {
     const uploadRef = doc(this.firestore, FirestorePath.Groups, group.id);
     const promise = setDoc(uploadRef, group)
-    .then(() => {
-      // success
-    })
-    .catch((err) => {
-      // toast error
-    });
-    return from(promise)
+      .then(() => {
+        // success
+      })
+      .catch((err) => {
+        // toast error
+      });
+    return from(promise);
+  }
+
+  getUserByID_OLD(id: string): Observable<User | undefined> {
+    const docRef = doc(this.firestore, FirestorePath.Users, id);
+    const promise = getDoc(docRef)
+      .then((docSnap: DocumentSnapshot) => {
+        return docSnap.data() as User;
+      })
+      .catch((err) => {
+        throw err;
+      });
+    return from(promise);
+  }
+
+  getUserByID(id: string): Observable<User | undefined> {
+    const docRef = doc(this.firestore, FirestorePath.Users, id);
+    return from(getDoc(docRef)).pipe(
+      map((docSnap: DocumentSnapshot) =>
+        docSnap.exists() ? (docSnap.data() as User) : undefined
+      )
+    );
   }
 }
