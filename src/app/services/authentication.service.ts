9dc500836799efd071d7564a003ae20f0a25b935
Manuel Braun <manuel.braun.coder@gmail.com>
2025-04-12 10:28:21 +0200

    add firebase setPersistence for session storage

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,46 +1,46 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, deleteUser, signInWithEmailAndPassword, signOut, UserCredential } from '@angular/fire/auth';
+import {
+  Auth,
+  browserSessionPersistence,
+  createUserWithEmailAndPassword,
+  deleteUser,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signOut,
+  UserCredential,
+} from '@angular/fire/auth';
 import { AuthUser } from '../interfaces/AuthUser';
 import { from, Observable } from 'rxjs';
 import { FirestoreService } from './firestore.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
   firestoreService = inject(FirestoreService);
   auth = inject(Auth);
   currentUserSig = signal<AuthUser | null | undefined>(undefined);
 
-  constructor() { }
+  constructor() {}
 
-  /**
-   * Register a new User and save him in firestore
-   * @param user AuthUser
-   * @returns an Observable with the UserCredential
-   */
-  register(user: AuthUser): Observable<UserCredential> {
-    const promise = createUserWithEmailAndPassword(this.auth, user.email, user.pwd!)
-    .then((userCredential: UserCredential) => {
-      this.firestoreService.saveUser(user, userCredential.user.uid);
-      this.setUserSignal(user);
-      return userCredential;
-    })
-    .catch((err) => {
-      throw err;
-    })
-    return from(promise)
+  register(user: AuthUser): Observable<void> {
+    const promise = setPersistence(this.auth, browserSessionPersistence)
+      .then(() => {
+        return createUserWithEmailAndPassword(this.auth, user.email, user.pwd!);
+      })
+      .then((userCredential: UserCredential) => {
+        this.firestoreService.saveUser(user, userCredential.user.uid);
+      })
+      .catch((err) => {
+        throw err;
+      });
+    return from(promise);
   }
 
-  login(user: AuthUser): Observable<UserCredential> {
-    const promise = signInWithEmailAndPassword(
-      this.auth,
-      user.email,
-      user.pwd!
-    )
-      .then((userCredential: UserCredential) => {
-        this.setUserSignal(user);
-        return userCredential
+  login(user: AuthUser): Observable<void> {
+    const promise = setPersistence(this.auth, browserSessionPersistence)
+      .then(() => {
+        signInWithEmailAndPassword(this.auth, user.email, user.pwd!);
       })
       .catch((err) => {
         throw err;
@@ -50,14 +50,14 @@ export class AuthenticationService {
 
   /**
    * Save User data in a signal
-   * 
+   *
    * @param user AuthUser
    */
   setUserSignal(user: AuthUser) {
     this.currentUserSig.set({
       email: user.email,
-      name: user.name
-    })
+      name: user.name,
+    });
   }
 
   logout(): Observable<void> {
@@ -68,7 +68,7 @@ export class AuthenticationService {
       .catch((err) => {
         // handle error
       });
-      return from(promise);
+    return from(promise);
   }
 
   deleteUser(): Observable<void> {
